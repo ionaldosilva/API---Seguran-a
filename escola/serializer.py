f388aa34cc0753aca96ea6c7cd199d8fd6d0ddae
Ionaldo Silva <ionaldo_araujo@hotmail.com>
2023-08-09 01:45:40 -0300

    config redis and caching

--- escola/serializer.py
+++ escola/serializer.py
@@ -4,7 +4,12 @@ from escola.models import Aluno, Curso, Matricula
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
-        fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
+        fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento','foto']
+
+class AlunoSerializerV2(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = ['id', 'nome', 'celular','rg', 'cpf', 'data_nascimento','foto']
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -31,7 +36,3 @@ class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
         model = Matricula
         fields = ['aluno_nome']
 
-class AlunoSerializerV2(serializers.ModelSerializer):
-    class Meta:
-        model = Aluno
-        fields = ['id', 'nome', 'celular','rg', 'cpf', 'data_nascimento']
--- setup/settings.py
+++ setup/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+MEDIA_URL  = '/media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
@@ -142,3 +144,16 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient'
+        }
+    }
+}
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_CACHE_ALIAS = 'default'
